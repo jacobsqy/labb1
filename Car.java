1a6f9a5b3d8ce24e03cdc2da70f768e1ba4203c2
jacobsqy <jacobsqy@zenbook.it>
2017-11-07 09:22:53 +0100

    jock was here

--- Car.java
+++ Car.java
@@ -4,18 +4,36 @@ public class Car {
   public double currentSpeed;
   public Color color;
   public String modelName;
+  public double turbo;
+  public boolean turboOn;
+  public double trimFactor;
+  public double turboPower;
   public boolean turbo;
 
 
 
 
+  public double speedFactor() {
+    double turbo = 1;
+    if(turboOn) turbo = turboPower;
+    return enginePower * 0.01 * trimFactor * turbo;
+  }
 
-
-
-
+  public void incrementSpeed(double amount){
+    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower)
+  }
+  public void decrementSpeed(double amount){
+      currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+  }
   /*GETTERS AND SETTERS*/
+  public void setTurboOn() {
+    turboOn = true;
+  }
+  public void setTurboOff() {
+    turboOn = false;
+  }
   public boolean getTurbo() {
-    return turbo;
+    return turboPower > 1;
   }
   public int getNrDoors(){
       return nrDoors;
@@ -43,5 +61,12 @@ public class Car {
   public void stopEngine(){
     currentSpeed = 0;
   }
+  /*public void incrementSpeed(double amount){
+      currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+  }
+
+  public void decrementSpeed(double amount){
+      currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+  }*/
 
 }
--- Saab95.java
+++ Saab95.java
@@ -3,42 +3,23 @@ import java.awt.*;
 public class Saab95{
 
     public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
 
     public Saab95(){
         nrDoors = 2;
         color = Color.red;
         enginePower = 125;
-	    turboOn = false;
+	      turboOn = false;
+        trimFactor = 1.0;
+        turboPower = 1.3;
         modelName = "Saab95";
         stopEngine();
       }
 
-    public void setTurboOn(){
-	    turboOn = true;
-    }
 
-    public void setTurboOff(){
-	    turboOn = false;
-    }
 
-    public double speedFactor(){
-        double turbo = 1;
-        if(turboOn) turbo = 1.3;
-        return enginePower * 0.01 * turbo;
-    }
 
-    public void incrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-    }
 
-    public void decrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-    }
+
 
     // TODO fix this method according to lab pm
     public void gas(double amount){
